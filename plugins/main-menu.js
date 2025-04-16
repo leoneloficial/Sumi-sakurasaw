a543ed74619d42d90dbd39dc6d5c7f07378dccb9
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-16 14:36:47 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -12,7 +12,7 @@ Hola! Soy  *${botname}*  ٩(˘◡˘)۶
 Aquí tienes la lista de comandos
 ╭┈ ↷
 │ᰔᩚ Cliente » @${userId.split('@')[0]}
-│❀ Modo » Publico
+│❀ Modo » privado
 │✦ Bot » ${(conn.user.jid == global.conn.user.jid ? 'Principal 🅥' : 'Prem Bot 🅑')}
 │ⴵ Activada » ${uptime}
 │✰ Usuarios » ${totalreg}
