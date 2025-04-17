bf0ffe0cf9797766e0ee7a513fa7378f137aa28a
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-17 02:16:24 -0400

    Actualizar jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -55,7 +55,7 @@ let handler = async (m, { conn: _envio, command, usedPrefix, args, text, isOwner
                 : `> para ser un subbot usa el comando *#code*\n\n> *Sub-Bots conectados: ${users.length}*`;
 
             const responseMessage = isMainBot
-                ? `❖ 𝗦𝗨𝗠𝗜 𝗦𝗔𝗞𝗨𝗥𝗔𝗦𝗔𝗪𝗔 \n${replyMessage}`
+                ? `❖ 𝗦𝗨𝘽 - 𝘽𝙊𝙏 𝙋𝙍𝙀𝙈𝙄𝙐𝙈 \n${replyMessage}`
                 : `${emoji} *ESTE ES UN SUB-BOT*\n${replyMessage}`;
 
             await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m})
