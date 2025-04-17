d6dd72fbd84b45e37b47f6cd1cb972aa4c266e90
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-17 17:22:33 -0400

    Actualizar tools-hd.js

--- plugins/tools-hd.js
+++ plugins/tools-hd.js
@@ -1,65 +1,53 @@
-import FormData from "form-data"
-import fetch from "node-fetch"
+import FormData from "form-data";
+import Jimp from "jimp";
+const handler = async (m, {conn, usedPrefix, command}) => {
+ try {    
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || q.mediaType || "";
+  if (!mime) return m.reply(`《✦》 Envie una imagen o responda a la imagen utilizando el comando: ${usedPrefix + command}`);
+  if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`《✧》 El formato del archivo (${mime}) no es compatible, envía o responda a una imagen`);
+  conn.reply(m.chat, '𖧷 Mejorando la calidad de la imagen....', m, {
+  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+  title: packname,
+  body: wm,
+  previewType: 0, thumbnail: icons,
+  sourceUrl: channel }}})
+  let img = await q.download?.();
+  let pr = await remini(img, "enhance");
+  conn.sendMessage(m.chat, {image: pr}, {quoted: fkontak});
+ } catch {
+ return m.reply("✪ Ocurrió un error");
+ }
+};
+handler.help = ["remini", "hd", "enhance"];
+handler.tags = ["ai", "tools"];
+handler.group = true;
+handler.register = false
+handler.command = ["remini", "hd", "enhance"];
+export default handler;
 
-const handler = async (m, { conn, usedPrefix, command }) => {
-  try {
-    let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || q.mediaType || ""
-
-    if (!mime) {
-      return m.reply(`❀ Por favor, envie una imagen o responda a la imagen utilizando el comando.`)
+async function remini(imageData, operation) {
+  return new Promise(async (resolve, reject) => {
+    const availableOperations = ["enhance", "recolor", "dehaze"];
+    if (availableOperations.includes(operation)) {
+      operation = operation;
+    } else {
+      operation = availableOperations[0];
     }
-
-    if (!/image\/(jpe?g|png)/.test(mime)) {
-      return m.reply(`✧ El formato del archivo (${mime}) no es compatible, envía o responde a una imagen.`)
-    }
-
-    conn.reply(m.chat, '❍ Mejorando la calidad de la imagen....', m)
-    let img = await q.download()
-    let url = await enhanceImage(img)
-    await conn.sendFile(m.chat, url, "out.png", "", fkontak)
-  } catch (error) {
-    return conn.reply(m.chat, `⚠︎ Ocurrió un error: ${error.message}`, m)
-  }
-}
-
-handler.help = ["hd"]
-handler.tags = ["tools"]
-handler.command = ["remini", "hd", "enhance"]
-handler.group = true
-
-export default handler
-
-async function enhanceImage(imageData) {
-  try {
-    const formData = new FormData()
-    formData.append("image", Buffer.from(imageData), {
-      filename: "enhance_image_body.jpg",
-      contentType: "image/jpeg"
-    })
-
-    const response = await fetch(
-      "https://inferenceengine.vyro.ai/enhance.vyro",
-      {
-        method: "POST",
-        body: formData,
-        headers: {
-          ...formData.getHeaders()
-        }
-      }
-    )
-
-    if (!response.ok) {
-      throw new Error(
-        `Error al procesar la imagen: ${response.status} - ${response.statusText}`
-      )
-    }
-
-    const result = await response.buffer()
-    return result
-  } catch (error) {
-    throw new Error(
-      `Error al mejorar la calidad de la imagen: ${error.message}`
-    )
-  }
+    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
+    const formData = new FormData();
+    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
+    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
+    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
+      function (err, res) {
+        if (err) reject(err);
+        const chunks = [];
+        res.on("data", function (chunk) {chunks.push(chunk)});
+        res.on("end", function () {resolve(Buffer.concat(chunks))});
+        res.on("error", function (err) {
+        reject(err);
+        });
+      },
+    );
+  });
 }
