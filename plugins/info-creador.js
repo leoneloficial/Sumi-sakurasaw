55f2e0c85a303d35b591b480edb614c866e1ec0d
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-18 00:09:39 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,7 +1,12 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-   await m.react('👑');
+    await m.react('👑');
+
+   
+    if (!['owner', 'creator', 'creador', 'dueño'].includes(command.toLowerCase())) {
+        return conn.sendMessage(m.chat, { text: `El comando ${command} no existe.` });
+    }
 
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     let name = await conn.getName(who);
